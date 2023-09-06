f9aee6e263cbb38df8a36e88baf6ee1afbcfb25a
henricoluca <118942982+henricoluca@users.noreply.github.com>
2023-09-06 16:43:59 -0300

    Update configFront.js

--- projeto-fase-2/frontend/src/function/configFront.js
+++ projeto-fase-2/frontend/src/function/configFront.js
@@ -1,10 +1,10 @@
 const config = {
-    apiUrl: "http://localhost:4000/frutas",
-    authUrl: "https://ec2-18-224-170-21.us-east-2.compute.amazonaws.com:8443/auth/oauth/v2/token",
-    clientId: "7f522efb-d91e-4c6d-88ba-d8d545a4d379", 
-    clientSecret: "80f65353-3565-4c5e-ab92-497376a3ba02", 
-    username: "enrico",
-    password: "8uGm0&H&",
+    apiUrl: "",
+    authUrl: "",
+    clientId: "", 
+    clientSecret: "", 
+    username: "",
+    password: "",
   };
   
 export default config;
